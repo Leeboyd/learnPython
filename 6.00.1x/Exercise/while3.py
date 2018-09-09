04bdb6347ad81d4ae1c38a847aa0453e4135469e
LeeBoy <jobboy19890101@gmail.com>
2018-09-09 21:02:00 +0800

    Exercise: while exercise 3

---
+++ 6.00.1x/Exercise/while3.py
@@ -0,0 +1,9 @@
+'''
+3. Write a while loop that sums the values 1 through end, inclusive. end is a variable that we define for you. So, for example, if we define end to be 6, your code should print out the result:
+'''
+i = 1
+j = 0
+while( i <= end ):
+    j = j + i
+    i+=1
+print(j)
