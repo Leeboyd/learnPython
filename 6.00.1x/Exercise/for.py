ab16c6f121200e09543d1a28d72fbe79e4d4cc28
LeeBoy <jobboy19890101@gmail.com>
2018-09-10 21:02:00 +0800

    Exercise: for exercise

---
+++ 6.00.1x/Exercise/for.py
@@ -0,0 +1,21 @@
+'''
+Week1: For exercise-1
+'''
+for i in range(2, 11, 2):
+    print(i)
+print('Goodbye!')
+
+'''
+Week1: For exercise-2
+'''
+print('Hello!')
+for i in reversed(range(2,11,2)):
+    print(i)
+
+'''
+Week1: For exercise-3
+'''
+j = 0
+for i in range(end + 1):
+    j = j + i
+print(j)
