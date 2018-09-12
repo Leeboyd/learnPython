218e2b3abde8f6ade2f26d8c2878e173b3be6837
LeeBoy <jobboy19890101@gmail.com>
2018-09-12 18:30:02 +0800

    fix: Pset-1 problem-3

--- 6.00.1x/Problem/PSET1.py
+++ 6.00.1x/Problem/PSET1.py
@@ -24,3 +24,30 @@ for index in xrange(len(s)):
     keywordOccurs += 1
   
 print('Number of times bob occurs is: ' + str(keywordOccurs))
+
+'''
+Pset-2 problem-2
+'''
+alpha = 'abcdefghijklmnopqrstuvwxyz'
+maxlength = 0
+tempSubstring = ''
+longestSubstring = ''
+
+for indI in range(len(s)):
+    # print('第', indI + 1, '輪始', tempSubstring, '挑戰者：', s[indI])
+    indexOfTestingChar = alpha.find(s[indI])
+    indexOfLastTestingChar =  alpha.find(s[indI - 1]) if indI - 1 >= 0 else 0 
+    
+    if (indI == 0 or len(tempSubstring) == 0):
+        tempSubstring += s[indI]
+        longestSubstring = tempSubstring
+    else:
+        if (indexOfTestingChar >= indexOfLastTestingChar):
+            tempSubstring += s[indI]
+            if (len(tempSubstring) > maxlength):
+                longestSubstring = tempSubstring
+                maxlength = len(longestSubstring)
+        else:
+            tempSubstring = s[indI]
+    # print('第', indI + 1, '輪尾的 tempSubstring', tempSubstring)
+print('Longest substring in alphabetical order is: ',  longestSubstring)
