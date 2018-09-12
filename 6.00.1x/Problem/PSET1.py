9b7086eb6e4bf2572e8dcb94d185579b0189e650
LeeBoy <jobboy19890101@gmail.com>
2018-09-12 11:32:00 +0800

    Pset-1: problem-2

--- 6.00.1x/Problem/PSET1.py
+++ 6.00.1x/Problem/PSET1.py
@@ -8,3 +8,19 @@ for char in s:
   if (char in vowels):
     countOfVowels+=1
 print('Number of vowels: ' + str(countOfVowels))
+
+'''
+Pset-2 problem-2
+'''
+# s = 'obobbobbobobbobobubobobbobobocrobobbobobo'
+keyword = 'bob'
+keywordOccurs = 0
+
+for index in xrange(len(s)):
+  stringTobeCheck = s[index:index+3]
+  if (len(stringTobeCheck) < 3):
+    break
+  if (s[index:index+3] == keyword):
+    keywordOccurs += 1
+  
+print('Number of times bob occurs is: ' + str(keywordOccurs))
