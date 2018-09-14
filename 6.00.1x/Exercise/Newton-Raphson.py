cfb6c660a396e49f523ba2a1676077b4d998012d
LeeBoy <jobboy19890101@gmail.com>
2018-09-14 22:20:00 +0800

    lecture: Newton-Raphson

---
+++ 6.00.1x/Exercise/Newton-Raphson.py
@@ -0,0 +1,14 @@
+'''
+Newton-Raphson
+'''
+
+epsilon = 0.01
+y = 578.0
+guess = y/2.0
+numGuesses = 0
+
+while abs(guess*guess - y) >= epsilon:
+    numGuesses += 1
+    guess = guess - (((guess**2) - y)/(2*guess))
+print('numGuess = ' + str(numGuesses))
+print('Square root of ' + str(y) + ' is about ' + str(guess))
