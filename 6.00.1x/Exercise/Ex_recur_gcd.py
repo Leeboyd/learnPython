db4bd83defa8a96ebe17f49ffa9403a16021e1c8
LeeBoy <jobboy19890101@gmail.com>
2018-09-20 00:52:30 +0800

    Exercise: gcd recur

---
+++ 6.00.1x/Exercise/Ex_recur_gcd.py
@@ -0,0 +1,8 @@
+def gcdRecur(a, b):
+    '''
+    a, b: positive integers
+    
+    returns: a positive integer, the greatest common divisor of a & b.
+    '''
+    # Your code here
+    return gcdRecur(b, a % b) if b else abs(a)
